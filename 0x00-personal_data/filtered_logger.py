e5e931a594970caf7d633550b24ae77a7d3bf984
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2024-07-04 06:36:21 +0300

    t2-3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,8 +9,15 @@
                           f'{field}={redaction}', message)
 """
 import logging
+from os import getenv
+from mysql.connector import connect
+from mysql.connector.pooling import PooledMySQLConnection
+from mysql.connector.abstracts import MySQLConnectionAbstract
 import re
-from typing import List
+from typing import List, Union
+
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'ip')
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -25,7 +32,7 @@ def filter_datum(fields: List[str], redaction: str,
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
-        """
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
@@ -40,3 +47,29 @@ class RedactingFormatter(logging.Formatter):
         """
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """ Returns a logging object
+        logger is named user_data and has a log level of INFO
+        It should not propagate messages to other loggers
+        It has a StreamHandler with RedactingFormatter as formatter
+    """
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+    return logger
+
+
+def get_db() -> Union[PooledMySQLConnection, MySQLConnectionAbstract]:
+    """ Returns a connector to the database
+    """
+    return connect(
+        user=getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        host=getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=getenv('PERSONAL_DATA_DB_NAME')
+    )
