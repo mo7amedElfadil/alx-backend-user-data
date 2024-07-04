acd311c48d88f28291de63ed5997d8f2d39ba54c
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2024-07-04 20:56:33 +0300

    explicit import of bcrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 """ Encrypt password using the bcrypt module
 """
-from bcrypt import checkpw, hashpw, gensalt
+import bcrypt
 
 
 def hash_password(password: str) -> bytes:
     """ Encrypts a password using bcrypt
+        Args:
+            password: a string to be encrypted
+        Returns:
+            a bytes object containing the encrypted password
     """
-    return hashpw(password.encode(), gensalt())
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """ Check if the password is valid
+        Args:
+            hashed_password: a bytes object containing the encrypted password
+            password: a string to be checked
+        Returns:
+            a boolean value indicating if the password is valid
     """
-    return checkpw(password.encode(), hashed_password)
+    return bcrypt.checkpw(password.encode(), hashed_password)
