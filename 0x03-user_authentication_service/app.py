e768c126dfcbda938a83fc35478290e48327d55b
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2024-07-18 00:56:03 +0300

    rm condition to check for form inputs

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -124,8 +124,6 @@ def update_password() -> str:
     email = request.form.get('email')
     reset_token = request.form.get('reset_token')
     new_password = request.form.get('new_password')
-    if not all([email, reset_token, new_password]):
-        return jsonify({'message': 'Bad request'}), 400
     try:
         AUTH.update_password(reset_token, new_password)
         return jsonify({'email': email, 'message': 'Password updated'}), 200
