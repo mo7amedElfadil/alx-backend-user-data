95625f2ea8bd2d2bf6819161e9c9c218d7b7c48d
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2024-07-18 04:32:53 +0300

    used logging to disable anything up to warning level

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ DB module
 """
+import logging
+from typing import Dict
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.exc import NoResultFound
@@ -8,17 +10,17 @@ from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from user import Base, User
-from typing import Dict
+
+logging.disable(logging.WARNING)
 
 
 class DB:
     """DB class
     """
-
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=False)
+        self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
