991ec22cc3742596154b964b4a8dd026bee08ce7
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2024-07-18 05:28:28 +0300

    User class

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -82,6 +82,6 @@ class DB:
         user = self.find_user_by(id=user_id)
 
         for key, value in kwargs.items():
-            if not hasattr(user, key):
+            if not hasattr(User, key):
                 raise ValueError
             setattr(user, key, value)
