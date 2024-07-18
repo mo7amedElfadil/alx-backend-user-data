9fc78123bd1943d4634d9b2feb3c7a3ff7e11f43
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2024-07-18 04:27:16 +0300

    pycs

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -109,6 +109,7 @@ def get_reset_password_token() -> str:
     except ValueError:
         abort(403)
 
+
 @app.route('/reset_password', methods=['PUT'])
 def update_password() -> str:
     """ PUT /reset_password route to update the user password.
