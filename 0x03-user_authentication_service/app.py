b8863c71ac8634803636303863eab04c8e02fb72
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2024-07-18 05:41:32 +0300

    rm status

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -132,7 +132,7 @@ def update_password() -> str:
         AUTH.update_password(reset_token, new_password)
     except ValueError:
         abort(403)
-    return jsonify({'email': email, 'message': 'Password updated'}), 200
+    return jsonify({"email": email, "message": "Password updated"})
 
 
 if __name__ == '__main__':
