acfc2b84ba8ca88c014ab3a475ba4b090ee306c5
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2024-07-19 04:42:17 +0300

    newlines

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -26,15 +26,16 @@ def users():
     """
     email = request.form.get("email")
     password = request.form.get("password")
+
     if not email or not password:
         abort(400)
+
     try:
         user = AUTH.register_user(email, password)
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
-    return jsonify({"email": user.email,
-                    "message": "user created"}), 200
+    return jsonify({"email": user.email, "message": "user created"}), 200
 
 
 @app.route("/sessions", methods=["POST"])
@@ -49,12 +50,14 @@ def login():
     """
     email = request.form.get("email")
     password = request.form.get("password")
+
     if not AUTH.valid_login(email, password):
         abort(401)
+
     session_id = AUTH.create_session(email)
-    response = jsonify({"email": email,
-                        "message": "logged in"})
+    response = jsonify({"email": email, "message": "logged in"})
     response.set_cookie("session_id", session_id)
+
     return response
 
 
@@ -67,10 +70,12 @@ def logout():
             - 302 and redirect to the main page if the session was deleted
     """
     session_id = request.cookies.get("session_id", None)
+
     if not session_id:
         abort(403)
 
     user = AUTH.get_user_from_session_id(session_id)
+
     if not user:
         abort(403)
 
